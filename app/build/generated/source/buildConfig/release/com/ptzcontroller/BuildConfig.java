1e33c0053b8ce598e9869b8d40c04dc3635d28ab
zforce0 <107778395+zforce0@users.noreply.github.com>
2025-05-08 13:44:24 +0800

    source files

---
+++ app/build/generated/source/buildConfig/release/com/ptzcontroller/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ptzcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.ptzcontroller";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String BUILD_TIMESTAMP = "1746547794700";
+  // Field from default config.
+  public static final String BUILD_VERSION_NAME = "1.0";
+}
